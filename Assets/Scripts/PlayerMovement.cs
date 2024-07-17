bb28f579154b70385592ee33368d273707111553
jadenhensley <jadencoding@gmail.com>
2024-07-17 13:26:28 -0400

    let there be square. almost working.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float speed       = 8f;
+    private float jump_force  = 10f;
+    private bool  is_grounded = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform   ground_check;
+    [SerializeField] private LayerMask   ground_layer;
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(ground_check.position, 0.2f, ground_layer);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_force);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+}
