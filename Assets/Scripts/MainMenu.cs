4ef0e1ee8550df3bad31dc4fa8650bf901781d73
Mary <maraltom@gmail.com>
2024-07-29 16:06:13 +0100

    can go to main scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject optionsMenu;
     public void Play()
     {
-
+        SceneManager.LoadScene(3);
     }
 
     public void Quit()
